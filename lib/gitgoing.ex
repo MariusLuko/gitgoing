93a388ee815d0b75a38ca0e3fe456ae80e2cf455
Mariusluko <lukosevicius.marius@gmail.com>
2020-05-02 14:32:40 -0700

    work from laptop

--- lib/gitgoing.ex
+++ lib/gitgoing.ex
@@ -16,6 +16,28 @@ defmodule Gitgoing do
     :world
   end
 
-  ## Adding a new comment to test recompiling
+  ## Testing adding values to a map
+  ## Don't like how I have to declare an empty map, should this go here?
 
+  def create_map do
+     map = %{}
+  end
+
+  def add_name(map) do
+     Map.put(map, :first_name, String.trim(IO.gets("What's your first name? ")))
+  end
+
+  def get(map, key) do
+     Map.get(map, key)
+  end
+
+#  customers = %{}
+#  Gitgoing.get_info()
+
+#  def get_info do
+#    fname = IO.gets("What's your first name? ")
+#    Map.put(customers, :first_name, fname)
+#    inspect customers
+#  end
+  
 end
