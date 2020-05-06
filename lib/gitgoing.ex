b12d131e93a98fc08aa70040cc181585182a0fcd
Mariusluko <lukosevicius.marius@gmail.com>
2020-05-05 18:31:55 -0700

    more changes

--- lib/gitgoing.ex
+++ lib/gitgoing.ex
@@ -12,21 +12,37 @@ defmodule Gitgoing do
       :world
 
   """
-  def hello do
-    :world
-  end
 
 
-  def create_map do
-     _map = %{}
+
+#  {:ok, socket} = :gen_udp.open(10110, [:binary, {:active, false}])
+#  :gen_udp.recv(socket,0)
+# {:ok, {ip, port, msg}} = :gen_udp.recv(socket,0)
+
+#  How can you get around needing to run "socket = Gitgoing.open_udp(10110)"?
+#  Have this be called automatically by other code...
+
+  def open_udp(port) do
+      case :gen_udp.open(port, [:binary, {:active, false}]) do
+         {:ok, socket} -> IO.puts("Socket #{inspect socket} has been opened.")
+                          socket
+      end
   end
 
-  def add_name(map) do
-     Map.put(map, :first_name, String.trim(IO.gets("What's your first name? ")))
+  def grab_packet(port) do
+     data = :gen_udp.recv(port,0)
+     IO.inspect(data)
+     parse(data)
   end
 
-  def get(map, key) do
-     Map.get(map, key)
+  defp parse(packet) do
+     case packet do
+        {:ok, {_ip, _port, msg}} -> IO.inspect(msg)
+        _ -> IO.puts("unexpected packet format")
+     end
   end
+
+  #  socket = Gitgoing.open_udp(10110)
+  #  Gitgoing.grab_packet(socket)
 
 end
